d5ee4119e7d5c56c062d327aa6ca472fc3465ada
Yanis Meziane <yanis.meziane@essec.edu>
2022-11-24 12:55:40 +0100

    init

--- pages/api/cron.ts
+++ pages/api/cron.ts
@@ -5,8 +5,8 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "POST") {
     try {
       res.status(200).json({ success: true });
-    } catch (err) {
-      res.status(500).json({ statusCode: 500, message: err.message });
+    } catch (error: any) {
+      res.status(500).json({ statusCode: 500, message: error.message });
     }
   } else {
     res.setHeader("Allow", "POST");
