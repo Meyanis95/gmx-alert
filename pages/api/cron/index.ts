81308e2375c38f1a977f7a1317ce10d8dc6fb6d7
Yanis Meziane <yanis.meziane@essec.edu>
2022-12-03 17:38:22 +0100

    fix bug on Qstash requests

--- pages/api/cron/index.ts
+++ pages/api/cron/index.ts
@@ -2,10 +2,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { cron } from "@/lib/cron";
 import { verifySignature } from "@upstash/qstash/nextjs";
 
-export default async function handler(
-  _req: NextApiRequest,
-  res: NextApiResponse
-) {
+async function handler(_req: NextApiRequest, res: NextApiResponse) {
   // if (await isDuplicateCron()) {
   //   // check if this is a duplicate cron job (threshold of 5s)
   //   return res.status(500).json({ message: "Duplicate cron job" });
@@ -28,7 +25,7 @@ export default async function handler(
  * To test out the endpoint manually (wihtout using QStash), you can do `export default handler` instead and
  * hit this endpoint via http://localhost:3000/api/cron
  */
-//export default verifySignature(handler);
+export default verifySignature(handler);
 
 export const config = {
   api: {
