6b2528b30c3d5149d74f3524fa0fedbce3bd25a0
Yanis Meziane <yanis.meziane@essec.edu>
2022-12-13 15:06:51 +0100

    added await before add tx in db

--- lib/cron.ts
+++ lib/cron.ts
@@ -50,7 +50,7 @@ export async function cron() {
           console.log(isInDb);
           if (!isInDb) {
             //If new trades store them + send notification on TG
-            addTxInDb(element.id);
+            await addTxInDb(element.id);
             sendMessage(element);
           }
         }
